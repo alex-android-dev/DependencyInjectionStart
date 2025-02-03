b9bcda5fe26d6de0cec8effd748488b4329d9dc4
avlnkov <alex.lenkov@mail.ru>
2025-02-03 17:13:01 +0300

    added class Component

--- app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
@@ -2,13 +2,8 @@ package com.example.dependencyinjectionstart.example1
 
 class Activity {
 
-    val monitor = Monitor()
-    val keyboard = Keyboard()
-    val mouse = Mouse()
-    val computerTower = ComputerTower(
-        Storage(),
-        Memory(),
-        Processor()
-    )
-    val computer = Computer(monitor, computerTower, keyboard, mouse)
+    val component = Component()
+
+    val computer = component.getComputer()
+
 }
