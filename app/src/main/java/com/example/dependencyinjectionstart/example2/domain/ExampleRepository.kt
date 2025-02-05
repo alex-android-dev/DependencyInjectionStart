c21fa1f8d41de1e221277139ae582dcf464efb8b
avlnkov <alex.lenkov@mail.ru>
2025-02-05 12:16:19 +0300

    добавил аннотацию Bind

--- app/src/main/java/com/example/dependencyinjectionstart/example2/domain/ExampleRepository.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/domain/ExampleRepository.kt
@@ -1,6 +1,5 @@
 package com.example.dependencyinjectionstart.example2.domain
 
 interface ExampleRepository {
-
     fun method()
 }
--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.dependencyinjectionstart.example2.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example1.Activity
+import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var viewModel: ExampleViewModel
+
+    val daggerComponent = DaggerApplicationComponent.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val activity = Activity()
         activity.computer
+        daggerComponent.inject(this)
+        viewModel.method()
     }
+
 }
